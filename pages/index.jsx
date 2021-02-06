0bbd0985666101d8dc28be91db96e8ac313afe60
Tatsuro Baba <harakirisoul@gmail.com>
2021-02-06 15:12:03 +0900

    Implement the first page of top

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>tbaba.info</h1>
+      </header>
+    </div>
+  );
+}
